ddf844666e3917fe8bd2ca70c6569a496819c419
welhazitakwa <welhazitakwa@gmail.com>
2022-11-19 22:42:51 +0100

    send response

--- Back_Node/index.js
+++ Back_Node/index.js
@@ -1,9 +1,8 @@
 const http = require('http');
 const app = require("./app");
 
-app.set('port',process.env.PORT || 3000)
-const server = http.createServer((req, res)=>{
-    res.end('we will do it ')
-});
+app.set('port', process.env.PORT || 3000)
+
+const server = http.createServer(app)
 
 server.listen(3000);
