713a8f78933e652ee48988fcf8ab021e7941641d
welhazitakwa <welhazitakwa@gmail.com>
2022-11-19 22:33:45 +0100

    update server to use express

--- Back_Node/index.js
+++ Back_Node/index.js
@@ -1,5 +1,7 @@
 const http = require('http');
+const app = require("./app");
 
+app.set('port',process.env.PORT || 3000)
 const server = http.createServer((req, res)=>{
     res.end('we will do it ')
 });
