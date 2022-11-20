6641787e0167241a74a2ad7cd67a9621755bb0a8
welhazitakwa <welhazitakwa@gmail.com>
2022-11-20 08:43:58 +0100

    connect to mongodb

--- Back_Node/app.js
+++ Back_Node/app.js
@@ -1,9 +1,27 @@
+const mongoose = require ('mongoose');
+
 const express = require('express');
 
 const app = express();
 
-app.use((req,res)=>{
+mongoose.connect('mongodb://localhost:27017/Power_Rangers',
+  { useNewUrlParser: true,
+    useUnifiedTopology: true
+  })
+  .then(() => console.log('Connected successfully to MongoDB !'))
+  .catch(() => console.log('Connection failed to MongoDB !'));
+
+  app.use(express.json());
+  app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+    next();
+  });
+
+/*
+app.get((req,res)=>{
     res.json('Hello World');
 })
-
+*/ 
 module.exports = app;
