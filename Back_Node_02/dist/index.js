4744810c37fb87a2d631bd973e7887d75a996dea
ChouroukAmara <chouroukamara114@gmail.com>
2022-11-20 12:02:44 +0100

    get post municipalite

--- Back_Node_02/dist/index.js
+++ Back_Node_02/dist/index.js
@@ -4,14 +4,36 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 exports.__esModule = true;
 var express_1 = __importDefault(require("express"));
+var municipalite_model_1 = __importDefault(require("./municipalite.model"));
 var mongoose_1 = __importDefault(require("mongoose"));
+var body_parser_1 = __importDefault(require("body-parser"));
 var app = (0, express_1["default"])();
+app.use(body_parser_1["default"].json());
 var url = "mongodb://localhost:27017/Power_Rangers";
 mongoose_1["default"].connect(url, function (err) {
     if (err)
         console.log(err);
     else
         console.log("Mongo Database connected successfuly");
+});
+/***************************get************************/
+app.get("/municipalites", function (req, resp) {
+    municipalite_model_1["default"].find(function (err, municipalites) {
+        if (err)
+            resp.status(500).send(err);
+        else
+            resp.send(municipalites);
+    });
+});
+/***************************post***********************/
+app.post("/municipalites", function (req, resp) {
+    var municipalite = new municipalite_model_1["default"](req.body);
+    municipalite.save(function (err) {
+        if (err)
+            resp.status(500).send(err);
+        else
+            resp.send(municipalite);
+    });
 });
 app.get("/", function (req, resp) {
     resp.send("hello express");
--- Back_Node_02/src/index.ts
+++ Back_Node_02/src/index.ts
@@ -1,8 +1,11 @@
-import  express from "express";
+import  express, {Request,Response} from "express";
+import Municipalite from "./municipalite.model";
 import mongoose from "mongoose";
+import bodyParser from "body-parser"
 
 
 const app=express();
+app.use(bodyParser.json())
 
 
 const url = "mongodb://localhost:27017/Power_Rangers"
@@ -10,6 +13,30 @@ mongoose.connect(url,(err)=>{
     if(err)console.log(err)
     else console.log("Mongo Database connected successfuly")
 });
+
+
+/***************************get************************/
+
+app.get("/municipalites",(req:Request,resp:Response)=>{
+    Municipalite.find((err,municipalites)=>{
+        if (err) resp.status(500).send(err);
+        else resp.send(municipalites);
+
+    })
+
+});
+
+/***************************post***********************/
+
+app.post("/municipalites",(req:Request,resp:Response)=>{
+    let municipalite = new Municipalite(req.body)
+    municipalite.save(err=>{
+        if(err) resp.status(500).send(err)
+        else resp.send(municipalite)
+    })
+
+});
+
 
 
 app.get("/" , (req,resp)=>{
