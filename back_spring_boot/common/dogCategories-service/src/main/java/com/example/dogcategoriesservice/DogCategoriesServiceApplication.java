98df7af9bce1a54fc63b32d18d9642528e45521e
mortadhasaidani <mortasaidani51@gmail.com>
2022-11-25 21:57:45 +0100

    create muli-model project

---
+++ back_spring_boot/common/dogCategories-service/src/main/java/com/example/dogcategoriesservice/DogCategoriesServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.dogcategoriesservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DogCategoriesServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DogCategoriesServiceApplication.class, args);
+    }
+
+}
---
+++ back_spring_boot/common/dogCategories-service/src/test/java/com/example/dogcategoriesservice/DogCategoriesServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.dogcategoriesservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DogCategoriesServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
