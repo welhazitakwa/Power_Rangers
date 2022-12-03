a0c1847c41931a2c04904320fabb7a1fe0b36742
ChouroukAmara <chouroukamara114@gmail.com>
2022-12-03 22:02:22 +0100

    update municipalite

--- argon-dashboard-angular-master/src/app/pages/update-municipalite/update-municipalite.component.ts
+++ argon-dashboard-angular-master/src/app/pages/update-municipalite/update-municipalite.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { FormBuilder } from '@angular/forms';
+import { ActivatedRoute, Router } from '@angular/router';
 import { MunicipaliteService } from 'src/app/services/municipalite.service';
 
 @Component({
@@ -15,13 +16,52 @@ export class UpdateMunicipaliteComponent implements OnInit {
     localisationMunicipalite:'',
       }
       currentMunicipalite=null;
+  id: any;
 
-  constructor(private municipaliteService: MunicipaliteService, private activatedRoute: ActivatedRoute) { }
+  constructor(private municipaliteService: MunicipaliteService, private activatedRoute: ActivatedRoute, private fb: FormBuilder, private routes :Router) { 
+  }
 
   ngOnInit(): void {
+    const data ={
+      nameMunicipalite: this.municipalite.nameMunicipalite,
+      localisationMunicipalite: this.municipalite.localisationMunicipalite
 
-    this.currentMunicipalite=this.municipaliteService.get(this.activatedRoute.snapshot.params['id']);
-    console.log(this.currentMunicipalite);
+    };
+
+    /*this.currentMunicipalite=this.municipaliteService.get(this.activatedRoute.snapshot.params['id']);
+    console.log(this.currentMunicipalite);*/
+    this.id = this.activatedRoute.snapshot.params['id'];
+    console.log(this.id);
+    this.municipaliteService.get(this.id).subscribe(
+      response =>{
+
+        console.log(response);
+        
+      },
+      error=> {
+        console.log(error);
+      }
+      
+    );
+  }
+
+  updatemunicipalite(){
+    const data ={
+      nameMunicipalite: this.municipalite.nameMunicipalite,
+      localisationMunicipalite: this.municipalite.localisationMunicipalite
+
+    };
+    this.municipaliteService.update(this.id,data).subscribe(
+      response =>{
+
+        console.log(response);
+        
+      },
+      error=> {
+        console.log(error);
+      }
+      
+    );
   }
 
 }
