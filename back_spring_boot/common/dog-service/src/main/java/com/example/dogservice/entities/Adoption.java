165d591bfd5d66134a5c74cab2a1a0d93f5227e3
mortadhasaidani <eyazribi089@gmail.com>
2022-12-31 15:29:06 +0100

    Fixed Adopte Dog

--- back_spring_boot/common/dog-service/src/main/java/com/example/dogservice/entities/Adoption.java
+++ back_spring_boot/common/dog-service/src/main/java/com/example/dogservice/entities/Adoption.java
@@ -5,17 +5,41 @@ import lombok.*;
 import javax.persistence.*;
 import java.sql.Date;
 
-@Table(name= "Demande")
+@Table(name= "Adoption")
 @Entity
-@Getter
-@Setter
-@AllArgsConstructor
 @NoArgsConstructor
-@ToString
 public class Adoption {
     @Id
+    @JoinColumn(name = "idDog", referencedColumnName = "idDog", insertable = false, updatable = false)
     private Long idDog;
-    @Id
     private Long idUser;
 
+    public Long getIdDog() {
+        return idDog;
+    }
+
+    @Override
+    public String toString() {
+        return "Adoption{" +
+                "idDog=" + idDog +
+                ", idUser=" + idUser +
+                '}';
+    }
+
+    public void setIdDog(Long idDog) {
+        this.idDog = idDog;
+    }
+
+    public Long getIdUser() {
+        return idUser;
+    }
+
+    public void setIdUser(Long idUser) {
+        this.idUser = idUser;
+    }
+
+    public Adoption(Long idDog, Long idUser) {
+        this.idDog = idDog;
+        this.idUser = idUser;
+    }
 }
