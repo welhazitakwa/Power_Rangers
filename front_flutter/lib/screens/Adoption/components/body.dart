09f9073281118e108083f73007ca736fc9ce509c
mortadhasaidani <eyazribi089@gmail.com>
2023-01-01 16:19:24 +0100

    AdoptionList FrontEnd

---
+++ front_flutter/lib/screens/Adoption/components/body.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+
+import '../../../size_config.dart';
+//import 'categories.dart';
+//import 'special_offers.dart';
+
+class Body extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: SingleChildScrollView(
+        child: Column(
+          children: [
+            /* SizedBox(height: getProportionateScreenHeight(20)),
+            HomeHeader(),
+            SizedBox(height: getProportionateScreenWidth(10)),
+            DiscountBanner(),
+            //Categories(),
+           // SpecialOffers(),
+            //SizedBox(height: getProportionateScreenWidth(10)),
+            PopularProducts(),
+            SizedBox(height: getProportionateScreenWidth(20)),*/
+          ],
+        ),
+      ),
+    );
+  }
+}
